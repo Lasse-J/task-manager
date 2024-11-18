bb7bebd1e3a06d0cbd7e5d29039908c8c5daa7da
Lasse Juusela <lasse.juusela@gmail.com>
2024-11-18 19:07:36 +0200

    Dark mode added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from '@/components/Header';
+import { ThemeProvider } from '@/components/theme-provider'
 
 export const metadata = {
   title: "Task Manager",
@@ -9,14 +10,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <div className="mx-auto w-full max-w-7xl animate-appear">
-          <Header />
-            <div className="px-4 py-2">
-              {children}
-            </div>
-        </div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >      
+          <div className="mx-auto w-full max-w-7xl animate-appear">
+            <Header />
+              <div className="px-4 py-2">
+                {children}
+              </div>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,14 @@ import { HomeIcon, File, UsersRound, Pencil } from 'lucide-react'
 import Link from 'next/link'
 
 import { NavButton } from '@/components/NavButton'
+import { ModeToggle } from '@/components/ModeToggle'
 
 export function Header() {
 	return (
 		<header className="animate-slide bg-background h-12 p-2 sticky top-0 z-20">
 			<div className="flex h-8 items-center justify-between w-full">
 				<div className="flex items-center gap-2">
+				
 					<NavButton href="/home" label="Home" icon={Pencil} />
 
 					<Link href="/home" className="flex justify-center items-center gap-2 ml-0" title="Home">
@@ -17,7 +19,13 @@ export function Header() {
 					</Link>
 				</div>
 				<div className="flex items-center">
-					<p>Right side</p>
+					
+					<NavButton href="/home" label="Home" icon={Pencil} />
+
+					<NavButton href="/home" label="Home" icon={Pencil} />
+				
+					<ModeToggle />
+
 				</div>
 			</div>
 		</header>
